c2d0c7b25b87c0455b0469f970728cf772e70bcd
Giri Prasad P <giriprasa@gmail.com>
2026-03-04 17:44:37 +0530

    assessment

--- variables-bharath.py
+++ variables-bharath.py
@@ -1,2 +1,44 @@
-list=(1,2,3,4)
-print(list, type(list))
+# Restaurant Order System
+
+# Scenario:
+# A restaurant wants to store a customer's order using Python data types.
+
+# Task:
+# Write a Python program that:
+
+# Store the customer name (string).
+
+# Store ordered items using a list.
+
+# Store unique item categories using a set.
+
+# Store price of each item using a dictionary.
+
+# Calculate the total bill amount.
+
+# Example Data
+
+# Items Ordered: ["Dosa", "Idli", "Coffee"]
+# Prices:
+# Dosa → 50
+# Idli → 30
+# Coffee → 20
+
+# Expected Output
+
+# Customer Name: Arun
+# Items Ordered: ['Dosa', 'Idli', 'Coffee']
+# Categories: {'Breakfast', 'Beverage'}
+# Total Bill: 100
+
+# Concepts Used
+
+# str
+
+# list
+
+# set
+
+# dict
+
+# int
