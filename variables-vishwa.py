cacc539ab43b4f7d5a18c4d8356e1f7d4e5e4a9e
Giri Prasad P <giriprasa@gmail.com>
2026-03-04 18:05:24 +0530

    Viswa assignment

--- variables-vishwa.py
+++ variables-vishwa.py
@@ -1,31 +1,62 @@
-Student Information System
+# Student Information System
 
-Scenario:
-A school wants to store information about a student using different Python data types.
+# Scenario:
+# A school wants to store information about a student using different Python data types.
 
-Task:
-Write a Python program that:
+# Task:
+# Write a Python program that:
 
-Stores the following details:
+# Stores the following details:
 
-Student Name (string)
+# Student Name (string)
+name = "Vishwa"
+# Age (integer)
+age = 20
+# Marks in 3 subjects (list)
+marks = [75,95,85]
+# Passed or Failed (boolean)
+pass_status = True
+# Store all details inside a dictionary.
+student = {
+    "name":name,
+    "age":age,
+    "marks":marks,
+    "result": pass_status
+}
+print(student)
+# Calculate the average marks.
+avg_mark = sum(marks)/len(marks)
 
-Age (integer)
+# Display all the student details and the average.
 
-Marks in 3 subjects (list)
+print(f"Student Name: {student['name']}")
+print(f"Student Age: {student['age']}")
+print(f"Student Mark: {student['marks']}")
+print(f"Student Average Score: {avg_mark}")
+print(f"Pass: {student['result']}")
 
-Passed or Failed (boolean)
 
-Store all details inside a dictionary.
+# Expected Output Example
 
-Calculate the average marks.
+# Student Name: Ravi
+# Age: 14
+# Marks: [78, 85, 90]
+# Average Marks: 84.33
+# Result: True
 
-Display all the student details and the average.
 
-Expected Output Example
+# ***************************************
+# Library Book Management
 
-Student Name: Ravi
-Age: 14
-Marks: [78, 85, 90]
-Average Marks: 84.33
-Result: True
+# Scenario:
+# A library wants to store information about books and check if a book is available.
+
+# Task
+
+# Write a Python program that:
+
+# Stores the library name (string).
+
+# Stores a list of books available.
+
+# Stores book availability status using a dictionary.
