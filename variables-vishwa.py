ea350c34aaab6cba66612212ac281088b8b0f298
Giri Prasad P <giriprasa@gmail.com>
2026-03-04 17:50:44 +0530

    Assignment

--- variables-vishwa.py
+++ variables-vishwa.py
@@ -1,2 +1,31 @@
-list=[1,2,3,4]
-print()
+Student Information System
+
+Scenario:
+A school wants to store information about a student using different Python data types.
+
+Task:
+Write a Python program that:
+
+Stores the following details:
+
+Student Name (string)
+
+Age (integer)
+
+Marks in 3 subjects (list)
+
+Passed or Failed (boolean)
+
+Store all details inside a dictionary.
+
+Calculate the average marks.
+
+Display all the student details and the average.
+
+Expected Output Example
+
+Student Name: Ravi
+Age: 14
+Marks: [78, 85, 90]
+Average Marks: 84.33
+Result: True
